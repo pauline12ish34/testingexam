c745fb1361316c2d40e4edb41c1578fd11a60f86
pauline12ish34 <paulineishimwe12@gmail.com>
2021-12-16 17:42:13 +0200

    other tests

--- Desktop/DevopsExam/termone/termoneexam-main/src/test/java/rw/ac/rca/termOneExam/controller/CityControllerIntegrationTest.java
+++ Desktop/DevopsExam/termone/termoneexam-main/src/test/java/rw/ac/rca/termOneExam/controller/CityControllerIntegrationTest.java
@@ -1,5 +1,9 @@
 package rw.ac.rca.termOneExam.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Objects;
+
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 //import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import rw.ac.rca.termOneExam.domain.City;
 
 
 @RunWith(SpringRunner.class)
@@ -23,7 +31,39 @@ public class CityControllerIntegrationTest {
 		String response = this.restTemplate.getForObject("/all", String.class);
 		
 		JSONAssert.assertEquals("[]", response, false);
-//		JSONAssert.assertEquals("[{id:101},{id:102},{id:103},{id:104}]", response, false);
+		JSONAssert.assertEquals("[{id:101},{id:102},{id:103},{id:104}]", response, false);
 	}
+	
+	
+	
+	@Test
+    public void create_testSuccess() {
+        City city1 = new City(20L,"Bugesera",10,200);
+        ResponseEntity<City> response = restTemplate.postForEntity("/add", city1, City.class);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals("dd", Objects.requireNonNull(response.getBody()).getName());
+    }
+    @Test
+    public void create_testDuplicateName() {
+        City city1 = new City(700,"Rutsiro",24,89);
+        ResponseEntity<String> response = restTemplate.postForEntity("/add", city1, String.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(HttpStatus.BAD_REQUEST,response.getStatusCode());
+    }
+    
+    @Test
+    public void findById_Success() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/102", String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void findById_testNotFound() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/109", String.class);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+    
 
 }
--- Desktop/DevopsExam/termone/termoneexam-main/src/test/java/rw/ac/rca/termOneExam/utils/CityUtilTest.java
+++ Desktop/DevopsExam/termone/termoneexam-main/src/test/java/rw/ac/rca/termOneExam/utils/CityUtilTest.java
@@ -1,5 +1,7 @@
 package rw.ac.rca.termOneExam.utils;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -26,4 +28,23 @@ public class CityUtilTest {
 	
 	}
 	
+	 @Test
+	    public void getDegree_notMore_than_40() {
+	        when(cityRepositoryMock.findAll()).thenReturn(Arrays.asList(new City(1,"Karongi",1,10)));
+	        assertTrue(40>cityService.getAll().get(0).getFahrenheit());
+	    }
+
+	    
+	    @Test
+	    public void getDegree_notless_than_10() {
+	        when(cityRepositoryMock.findAll()).thenReturn(Arrays.asList(new City(1,"Karongi",1,10)));
+	        assertTrue(10< cityService.getAll().get(0).getFahrenheit());
+	    }
+	
+	    @Test
+	    public void checkMusanz() {
+	        when(cityRepositoryMock.findAll()).thenReturn(Arrays.asList(new City(1,"Karongi",1,10),new City(1,"Kigali",1,10),new City(1,"Musanze",1,10)));
+	        assertEquals("Musanze",cityService.getAll().get(2).getName());
+	        assertEquals("Musanze",cityService.getAll().get(2).getName());
+	    }
 }
